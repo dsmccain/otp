566c547e6b024b891e769f41b7b1b46932853486
Magnus Lång <margnus1@telia.com>
2014-04-21 15:15:54 +0200

    Added case when failure labels are function_clause
    They have the particular trait of going backwards in the BEAM rather
    than forwards, exposing bugs with multiply defined failure labels.

--- lib/hipe/test/maps_SUITE_data/maps_put_map_assoc.erl
+++ lib/hipe/test/maps_SUITE_data/maps_put_map_assoc.erl
@@ -7,9 +7,15 @@ test() ->
     #{a:=true} = assoc_update(#{}),
     {'EXIT', {badarg, [{maps_put_map_assoc, assoc_update, 1, _}|_]}}
 	= (catch assoc_update(not_a_map)),
+    ok = assoc_guard_clause(#{}),
+    {'EXIT', {function_clause, [{maps_put_map_assoc,
+				 assoc_guard_clause, _, _}|_]}}
+	= (catch assoc_guard_clause(not_a_map)),
     ok.
 
 assoc_guard(M) when is_map(M#{a=>b}) -> true;
 assoc_guard(_) -> false.
 
 assoc_update(M) -> M#{a=>true}.
+
+assoc_guard_clause(M) when is_map(M#{a=>3}) -> ok.
--- lib/hipe/test/maps_SUITE_data/maps_put_map_exact.erl
+++ lib/hipe/test/maps_SUITE_data/maps_put_map_exact.erl
@@ -10,9 +10,18 @@ test() ->
 	= (catch exact_update(not_a_map)),
     {'EXIT', {badarg, [{maps_put_map_exact, exact_update, 1, _}|_]}}
 	= (catch exact_update(#{})),
+    ok = exact_guard_clause(#{a=>yes}),
+    {'EXIT', {function_clause, [{maps_put_map_exact,
+				 exact_guard_clause, _, _}|_]}}
+	= (catch exact_guard_clause(#{})),
+    {'EXIT', {function_clause, [{maps_put_map_exact,
+				 exact_guard_clause, _, _}|_]}}
+	= (catch exact_guard_clause(not_a_map)),
     ok.
 
 exact_guard(M) when is_map(M#{a:=b}) -> true;
 exact_guard(_) -> false.
 
 exact_update(M) -> M#{a:=true}.
+
+exact_guard_clause(M) when is_map(M#{a:=3}) -> ok.
